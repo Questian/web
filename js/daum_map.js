6de17ad7aa9d4f5775c4ffd23670158166658f92
Philip YS <wbvcos@gmail.com>
2016-01-28 03:02:26 +0900

    move daum map & add button js

---
+++ js/daum_map.js
@@ -0,0 +1,6 @@
+var container = document.getElementById('daum-map');
+var options = {
+  center: new daum.maps.LatLng(34.693318, 135.501592),
+  level: 3
+};
+var map = new daum.maps.Map(container, options);
