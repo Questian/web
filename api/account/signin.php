a7e701f5e9b5ff2b79f0f7292cb2a77d39ddd3d3
endless_hack <ysw0094@gmail.com>
2016-01-28 19:30:25 +0900

    Map bug fix && signup update

--- api/account/signin.php
+++ api/account/signin.php
@@ -1,12 +1,18 @@
 <?php
-  include './db_conncect.php';
-  $id = $_POST['id'];
-  $pw = $_POST['password'];
-  
-  if(!empty($id) || !empty($pw)){
-    $signin_result = mysql_query('SELECT * FROM users where id =' .$id .'and where password = '.$pw);
-  } else {
-    //less parameter
-  }
-  
+session_start();
+include './db_connect.php';
+
+$id = mysql_real_escape_string($_POST['id']);
+$password = md5(mysql_real_escape_string($_POST['password']));
+$email = mysql_real_escape_string($_POST['email']);
+$name = mysql_real_escape_string($_POST['username']);
+
+if(mysql_query("INSERT INTO users(id,username, email, password) VALUES('$id','$username','$email', '$password')")){
+	//register succeed
+	echo "register succeed";
+} else {
+	//register failed
+	echo "register failed";
+}
+
 ?>
--- js/daumMap/daum_map.js
+++ js/daumMap/daum_map.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('daum-map');
 var options = {
-  center: new daum.maps.LatLng(34.693318, 135.501592),
-  level: 3
+  center: new daum.maps.LatLng(35.146856, 126.8383145),
+  level: 4
 };
 var map = new daum.maps.Map(container, options);
