7c5ad4943e401714666b6853f1538a6ce575c697
Seungwoo Yeom <ysw0094@gmail.com>
2016-06-29 19:58:29 +0900

    Start using codeigniter (initialize project)

--- js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $.toast.config.align = 'right';
-    $.toast.config.width = 400;
-
-    $('button').click(function(){
-        createToast($(this).attr('class'));
-    });
-});
--- js/index.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Seungwoo on 2016. 3. 20..
- */
